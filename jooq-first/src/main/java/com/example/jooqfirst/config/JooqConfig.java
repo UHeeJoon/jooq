beaaff379c0c77a6d6e8668995764024f43622a0
UHeeJoon <yhjyhj3855@gmail.com>
2025-02-27 14:25:32 +0900

    [#6] feat: slow query 감지 설정 추가

--- jooq-first/src/main/java/com/example/jooqfirst/config/JooqConfig.java
+++ jooq-first/src/main/java/com/example/jooqfirst/config/JooqConfig.java
@@ -16,13 +16,16 @@ public class JooqConfig {
 
   @Bean
   public DefaultConfigurationCustomizer configurationCustomizer() {
-    return c -> c.settings()
-      // 조건절 없이 delete 할 경우 throw
-      .withExecuteDeleteWithoutWhere(ExecuteWithoutWhere.THROW)
-      // 조건절 없이 update 할 경우 throw
-      .withExecuteUpdateWithoutWhere(ExecuteWithoutWhere.THROW)
-      // 쿼리에서 스키마 이름 빼고 나오게 하기
-      .withRenderSchema(false);
+    return c -> {
+      c.set(PerformanceListener::new);
+      c.settings()
+        // 조건절 없이 delete 할 경우 throw
+        .withExecuteDeleteWithoutWhere(ExecuteWithoutWhere.THROW)
+        // 조건절 없이 update 할 경우 throw
+        .withExecuteUpdateWithoutWhere(ExecuteWithoutWhere.THROW)
+        // 쿼리에서 스키마 이름 빼고 나오게 하기
+        .withRenderSchema(false);
+    };
   }
 
 }
