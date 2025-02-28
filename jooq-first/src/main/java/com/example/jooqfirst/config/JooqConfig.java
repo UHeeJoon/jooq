292226be03b39be1ec0572bde8fd1fb2f0dcf05c
YooHeeJoon <92840513+UHeeJoon@users.noreply.github.com>
2025-02-28 11:48:19 +0900

    [#8] feat: path-based join 으로 쿼리 최적화

--- jooq-first/src/main/java/com/example/jooqfirst/config/JooqConfig.java
+++ jooq-first/src/main/java/com/example/jooqfirst/config/JooqConfig.java
@@ -1,6 +1,7 @@
 package com.example.jooqfirst.config;
 
 import org.jooq.conf.ExecuteWithoutWhere;
+import org.jooq.conf.RenderImplicitJoinType;
 import org.springframework.boot.autoconfigure.jooq.DefaultConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,7 +25,16 @@ public class JooqConfig {
         // 조건절 없이 update 할 경우 throw
         .withExecuteUpdateWithoutWhere(ExecuteWithoutWhere.THROW)
         // 쿼리에서 스키마 이름 빼고 나오게 하기
-        .withRenderSchema(false);
+        .withRenderSchema(false)
+
+
+        // implicit path join to-many는 기본적으로 에러를 발생시켜 이렇게 수동으로 조인을 지정 해야한다.
+        .withRenderImplicitJoinToManyType(RenderImplicitJoinType.INNER_JOIN)
+
+        // implicit PATH JOIN many-to-one 을 비활성화 하고 싶다면 하고 싶다면
+//        .withRenderImplicitJoinType(RenderImplicitJoinType.THROW)
+
+      ;
     };
   }
 
